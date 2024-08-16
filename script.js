719bfdef3bfae2afc3322ac74b22f9e2ed756576
NeyleCM <neylecmorantes@gmail.com>
2024-08-16 23:41:44 +0200

    3 códigos

--- script.js
+++ script.js
@@ -21,7 +21,30 @@ const blurring = () =>  {
 
 let int = setInterval(blurring, 30)
 
-/*
+
+/* ---- IGUAL PERO CON LA FUNCIÓON DENTRO DEL SET INTERVAL  ------
+
+const loading = document.querySelector('.loading')
+const mainBg = document.querySelector('.mainBg')
+let load = 0
+
+let int = setInterval(() => {
+    load++
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loading.innerHTML = `${load}%`;
+    loading.style.opacity = scale(load, 0, 100, 1, 0)
+    mainBg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}, 30)
+
+const scale = (num, inMin, inMax, outMin, outMax) =>
+    ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
+*/
+
+
+/*  ----- DIFERENTE - CON OPACITY -----
+
 window.onload = function() {
     const loading = document.getElementById('loading');
     const mainBg = document.getElementById('mainBg');
